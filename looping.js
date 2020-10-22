33178ed39971718effd81c7cdaa014f2cb02cbb3
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2020-10-22 13:30:58 +0100

    updating and adding new examples

--- looping.js
+++ looping.js
@@ -1,59 +1,70 @@
-function numbers(){
-//if the var count is in the for loop then it will get put back to zero
-  var count = 0;
-  for (var i = 0; i <= 100; i++){
+//To run this file and see output to the terminal run node looping.js
+
+function numbersLoop(){
+//if the let count is in the for loop then it will get put back to zero
+  let count = 0;
+  for (let i = 0; i <= 100; i++){
     count += i;
     console.log(i);
   }
 //this kicks out the result of the for loop
   return count;
-}
+};
 
-function numbers(){
-//if the var count is in the for loop then it will get put back to zero
-  var count = 0;
-  for (var i = 0; i <= 100; i++){
+console.log(numbersLoop());
+
+function numbersLoopForMulitplesOfThree(){
+//if the let count is in the for loop then it will get put back to zero
+  let count = 0;
+  for (let i = 0; i <= 100; i++){
 //this if is just saying that is wants the numbers that are % by 3
-    if (i%3===0){
+    if (i%3 === 0){
     count += i;
     }
   }
 //this kicks out the result of the for loop
   return count;
-}
+};
 
-var stuff = ["apples","bananas","fish","corn"];
-shop(stuff);
+console.log(numbersLoopForMulitplesOfThree());
+
+const stuff = ["apples", "bananas", "fish", "corn"];
+
 //shopping is just a placeholder for whatever you are passing through when calling it
-function shop(shopping){
-//if the var count is in the for loop then it will get put back to zero
-  var count = 0;
-  for (var i = 0; i < shopping.length; i++){
+function shopForApricots(shopping){
+//if the let count is in the for loop then it will get put back to zero
+  let count = 0;
+  for (let i = 0; i < shopping.length; i++){
 //this if is tells me how many apricots i have
-    if ("apricots"===shopping[i]){
+    if ("apricots" === shopping[i]){
     count ++;
     }
   }
 //this kicks out the result of the for loop
   return count;
-}
+};
 
-var basket = ["apples","bananas","bananas","bananas","fish","corn"];
+console.log(shopForApricots(stuff));
 
-function shop(shopping,nameOfItem){
-//if the var count is in the for loop then it will get put back to zero
-  var count = 0;
-  for (var i = 0; i < shopping.length; i++){
+const basket = ["apples","bananas","bananas","bananas","fish","corn"];
+
+function shopForBannas(shopping, nameOfItem){
+//if the let count is in the for loop then it will get put back to zero
+  let count = 0;
+  for (let i = 0; i < shopping.length; i++){
 //this if is tells me to look through the basket and pull out the nameOfItem.
-    if (nameOfItem===shopping[i]){
+    if (nameOfItem === shopping[i]){
     count ++;
-  }
+    }
   }
 //this kicks out the result of the for loop
   return count;
-}
+};
 //this calls the function and looks for just bananas
-shop(basket."bananas");
+console.log(shopForBannas(basket , "bananas"));
+
+
+const myNumbers = [3,1,5,8,1,9,7,8888,8,4];
 
 // sort the array from lowest to highest
 function sort(arr){
@@ -68,4 +79,35 @@ function sort(arr){
     }
   }
   return arr;
-}
+};
+
+console.log(sort(myNumbers));
+
+const forEachnumbers = [1, 2, 3, 4, 5];
+
+//using a fatArrow instead of a normal function
+forEachnumbers.forEach((number) => {
+  //number can be named to anything ie fish
+    console.log(number);
+});
+
+const arrayBeforeBeingSquared = [2, 5, 9];
+//map takes in one element of the array in at a time and then acts upon it 
+let arrayAfterBeingsquared = arrayBeforeBeingSquared.map((num) => num * num);
+
+console.log(arrayBeforeBeingSquared);
+console.log(arrayAfterBeingsquared);
+
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+//filter only returns values that match the condition in the function
+const wordsFiltered = words.filter(word => word.length > 6);
+
+console.log(wordsFiltered);
+
+const array1 = [1, 2, 3, 4];
+const total = array1.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue
+},2);
+
+console.log(total);
